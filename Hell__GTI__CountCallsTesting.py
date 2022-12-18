2a291fcaa6d6f27324417d89586ae319386d2ff9
Rah-Jose-Israel <jossorio-pro@outlook.com>
2022-12-17 19:42:52 -0500

    Enter A new grad With literally no idea what the shit to do with his life. So this means just we do something, This is that something.

--- Hell__GTI__CountCallsTesting.py
+++ Hell__GTI__CountCallsTesting.py
@@ -1,7 +1,4 @@
 from main import CountCalls
-import timeit
-import logging
-
 
 print("This Challenge is for myself to import. \n Goal One : Beat the Challenges 1-100 with python to the best of "
       "your ability \n Goal Two  : Beat the Challenges 1-100 with cython to the best of "
@@ -16,7 +13,8 @@ So this means just we do something, This is that something.     - {Rahenvy : Jos
 
 def test_compare(select_RangeValueFirst,select_RangeValueSecond):
     """Qestion One"""
-    f1 = select_RangeValue=CountCalls.D7NM5(range(int(select_RangeValueFirst), int(select_RangeValueSecond)))
+
+    f1 = select_RangeValue=CountCalls().D7NM5(range(int(select_RangeValueFirst), int(select_RangeValueSecond)))
     if (x for x in f1 if x % 7 != 0 and x % 5 == 0):
         return AssertionError(f"your args return Incorrect {(x for x in f1 if f1 % 7 != 0 and f1 % 5 == 0)}")
     print(f"Final Result = {select_RangeValue.D7NM5(range(select_RangeValueFirst, select_RangeValueSecond))}")
--- logging_TimeTrials.py
+++ logging_TimeTrials.py
@@ -7,14 +7,14 @@ import Hell__GTI__CountCallsTesting
 logging.basicConfig(filename='100_PythonChallenging_LOG.log', encoding='utf-8', level=logging.INFO)
 lowVal, highVals = input("Your low number?"), input("Your high number?")
 starttime = timeit.default_timer()
-Hell__GTI__CountCallsTesting.test_compare(lowVal, highVals)
+inter_CostumRange = Hell__GTI__CountCallsTesting.test_compare(lowVal, highVals)
 endtime = timeit.default_timer()
 
 
 Question_OneTestResults = {"Problem Number":"Question 1",
                 "Test Start Time": str(starttime),
                "Low Value": str(lowVal) ,
-               "High Value": str(highVals) ,
+               "High Value": str(highVals) ,"Range":inter_CostumRange,
                "Time Difference"  : timeit.default_timer() - starttime
                }
 logging.info(str(Question_OneTestResults))
